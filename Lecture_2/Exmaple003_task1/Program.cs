a8f713da48315938730189ec56ee554bfe59e39a
Oppj4 <geed704@gmail.com>
2023-01-18 13:45:31 +0300

    Task1 in Lecture_2

---
+++ Lecture_2/Exmaple003_task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задание1:
+// Имеется одномерный массив array из n элементов,
+// требуется найти элемент, равный find.
+
+int[] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
+
+int arrayLen = array.Length;
+int index = 0;
+
+int find = 15;
+
+while(index < arrayLen){
+    if(array[index] == find){
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+};
