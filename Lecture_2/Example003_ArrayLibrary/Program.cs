4c8eddf6bad301e44db15461488403e08a4d2372
Oppj4 <geed704@gmail.com>
2023-01-18 14:18:34 +0300

    Example003

---
+++ Lecture_2/Example003_ArrayLibrary/Program.cs
@@ -0,0 +1,42 @@
+﻿void FillArray(int[] collection)
+{
+    int arrayLength = collection.Length;
+    int index = 0;
+    while (index < arrayLength){
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col){
+    int count = col.Length;
+    int position = 0;
+    while(position < count){
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find){
+    int count = collection.Length;
+    int FindInd = -1;
+    int position = 0;
+    
+    while(position < count){
+        if(collection[position] == find){
+            FindInd = position;
+            break;
+        }
+        position++;
+    }
+    return FindInd;
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int resultInd = IndexOf(array, 4);
+if(resultInd != -1) Console.WriteLine(resultInd);
+else Console.WriteLine("В массиве нету искомого числа!");
