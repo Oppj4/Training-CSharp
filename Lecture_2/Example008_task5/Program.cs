e5fac669ac9fd8467aa4330da1933b974bc3e720
Oppj4 <geed704@gmail.com>
2023-01-19 15:53:43 +0300

    task5

---
+++ Lecture_2/Example008_task5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача: 
+// Напишите программу, которая принимает на вход число
+// и проверяет, кратно ли оно одновременно 7 и 23.
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string result = "";
+
+if(num == 0) result = "На ноль делить нельзя!";
+else if (num % 7 == 0 && num % 23 == 0) result = $"Число {num} КРАТНО 7 и 23 одновременно"; 
+else result = $"Число {num} НЕ КРАТНО 7 и 23 одновременно";
+
+Console.WriteLine(result);
