5fd92701ec3d2a13586c65fa470342981d179773
Oppj4 <geed704@gmail.com>
2023-01-19 14:50:57 +0300

    Task3

---
+++ Lecture_2/Example006_task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача: 
+// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+
+string firstDigit = Convert.ToString(number / 100);
+string secondDigit = Convert.ToString(number % 10);
+
+string result = firstDigit + secondDigit;
+Console.WriteLine($"Изначальное число: {number} После операции: {result}");
+
+
+
+
