ddfce23e1f9de1889778e1c3822b5ba4692a1fb0
Oppj4 <geed704@gmail.com>
2023-01-19 16:12:48 +0300

    task6

---
+++ Lecture_2/Example009_task6/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача: 
+// Напишите программу, которая принимает на вход
+// два числа и проверяет, является ли одно число
+// квадратом другого.
+// 5, 25 -> да 
+// -4, 16 -> да
+// 25, 5 -> да
+// 8, 9 -> нет
+
+Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+bool isDiv1 = numA % numB == 0 ? true : false;
+bool isDiv2 = numB % numA == 0 ? true : false;
+
+string result = "";
+if (isDiv1 || isDiv2) result = $"Одно из чисел [{numA} {numB}] кратно другому.";
+else result = "Ни одно из чисел [{numA, numB} не кратно друг другу.";
+
+Console.WriteLine(result);
