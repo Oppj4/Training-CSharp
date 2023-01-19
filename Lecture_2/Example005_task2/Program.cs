e4a3a41a980aee523e41d14d152584a049199c03
Oppj4 <geed704@gmail.com>
2023-01-19 14:37:34 +0300

    rename Task1 and create Task2

---
+++ Lecture_2/Example005_task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача: 
+// Написать программу, которая 
+// Выводит случайное число из отрезка [10, 99] и
+// показывает наибольшую цифру числа.
+
+
+int number = new Random().Next(10, 100);
+int result = 0;
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+
+if(firstDigit >= secondDigit) result = firstDigit;
+else result = secondDigit;
+
+
+Console.WriteLine($"Самая большая цифра числа {number} = {result}");
