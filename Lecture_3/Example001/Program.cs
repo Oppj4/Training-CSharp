a32d18470eba8eadf38796265b41071be3fba87e
Oppj4 <geed704@gmail.com>
2023-01-22 13:54:10 +0300

    Example001

---
+++ Lecture_3/Example001/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача:
+// Вывод таблицы умножения
+
+for(int i = 2; i <= 9; i++)
+{   
+    for(int j = 1; j <= 9; j++)
+    {
+        Console.WriteLine($"{i} X {j} = {i * j}");
+    }
+    Console.WriteLine($"     ==========[{i}]==========");
+}
