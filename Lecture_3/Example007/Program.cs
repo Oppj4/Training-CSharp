76bfaa12ffc2df42468bd47b2ef8fc42c1c136ab
Oppj4 <geed704@gmail.com>
2023-01-23 19:40:34 +0300

    Example008

---
+++ Lecture_3/Example007/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача:
+// Напишите программу, которая принимает
+// на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void SquareNums(int n)
+{
+    int temp = n;
+    if(temp < 0){
+        temp *= -1;
+    }
+
+    for(int i = 1; i <= temp; i++)
+    {   
+        if(num != temp)
+        {
+            Console.Write($"| ");
+            Console.WriteLine($"Квадрат числа -{i} -> {Math.Pow(i, 2)}");
+        }
+        else
+        {
+            Console.Write($"| ");
+            Console.WriteLine($"Квадрат числа {i} -> {Math.Pow(i, 2)}");
+        }
+    }
+}
+SquareNums(num);
