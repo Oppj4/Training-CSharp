1fb6c04f7ef00bfd89827c03e9011ca28e96b027
Oppj4 <geed704@gmail.com>
2023-01-23 15:43:16 +0300

    Example006

---
+++ Lecture_3/Example005/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача:
+// Напишите программу, которая по заданному номеру
+// четверти, показывает диапозон возможных координат
+// точек в этой четверти(x и y)
+
+Console.Write("Введите номер четверти: ");
+int numQuart = Convert.ToInt32(Console.ReadLine());
+
+string rangeCors(int Quart)
+{
+    if(Quart == 1) return "[x > 0 и y > 0]";
+    if(Quart == 2) return "[x < 0 и y > 0]";
+    if(Quart == 3) return "[x < 0 и y < 0]";
+    if(Quart == 4) return "[x < 0 и y < 0]";
+    return "Введены неверные данные";
+    
+}
+
+Console.WriteLine(rangeCors(numQuart));
