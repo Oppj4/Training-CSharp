20eacbb8a8ba19969852847fc2949c30b81de642
Oppj4 <geed704@gmail.com>
2023-03-10 18:54:39 +0300

    Lecture_4 task3

---
+++ Lecture_4/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+double factorial(int n)
+{
+    if(n == 1) return 1;
+    else return n * factorial(n - 1);
+    
+}
+
+
+Console.WriteLine(factorial(num));
