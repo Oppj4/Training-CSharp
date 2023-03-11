2bf669ececd582cac5570bcda4331ac54dfbbd91
Oppj4 <geed704@gmail.com>
2023-03-11 20:27:45 +0300

    Lecture_4 task5

---
+++ Lecture_4/task5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 5.
+// Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 278 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int counter = 0;
+int number_of_digits(int num)
+{
+    while(num != 0)
+    {
+        num /= 10;
+        counter+=1;
+    }
+return counter;
+
+}
+
+Console.WriteLine(number_of_digits(num));
