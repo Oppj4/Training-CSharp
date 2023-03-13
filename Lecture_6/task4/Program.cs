125cfc9806b8a226af85e24ba1e78094b5bb0c31
Oppj4 <geed704@gmail.com>
2023-03-13 14:16:05 +0300

    Lecture 6 task 4

---
+++ Lecture_6/task4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 4.
+// Напишите программу, которая будет преобразовать
+// десятичное число в двоичное.
+
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+Console.WriteLine("Введите десятичное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+int  DecimalToBinary(int num)
+{
+    int result = default;
+    int mult = 1;
+    while (num > 0)
+    {
+        result += num % 2 * mult; 
+        num /= 2;
+        mult *= 10;
+    }
+
+    return result;
+}
+
+Console.WriteLine(DecimalToBinary(num));
